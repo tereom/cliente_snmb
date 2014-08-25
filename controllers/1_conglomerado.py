3f048eabb7dfb2420922c058528a58a9a2c6c647
fpardourrutia <firedragon_fernando@hotmail.com>
2014-08-25 18:51:44 -0500

    Conglomerado ya sirve

--- controllers/1_conglomerado.py
+++ controllers/1_conglomerado.py
@@ -4,7 +4,12 @@ def vegetation_typeCheck(form):
     if form.vars.soil_use_type==T('Vegetación') and not form.vars.vegetation_type:
         form.errors.vegetation_type=T("Debe elegir un tipo de vegetación predominante")
 
-def  
+def controladorConglomerado():
+    form = SQLFORM(db.Conglomerate_sample)
+    if form.process(onvalidation=vegetation_typeCheck).accepted:
+        response.flash = "success"
+    return dict(form=form)
+
 #def check(form):
 #    if form.vars.b and not form.vars.c:
 #        form.errors.c = "If the b is checked, c must be filled"
