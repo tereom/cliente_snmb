39503904dbb3a38ca42d4c3cab304e8b837bf534
fpardourrutia <firedragon_fernando@hotmail.com>
2014-08-26 18:46:02 -0500

    Avance parcial pestaña 1 completa.

--- controllers/1_conglomerado.py
+++ controllers/1_conglomerado.py
@@ -3,25 +3,26 @@
 def additionalValidations(congform):
     #If soil_use_type is not 'Vegetación' then is_perturbed=None
     if not congform.vars.soil_use_type==T('Vegetación'):
-        congform.errors.is_perturbed=None
+        congform.vars.vegetation_type=None
+        congform.vars.is_perturbed=None
     #Checking if the vegetation_type control is filled when soil_use_type='Vegetación'
     elif not congform.vars.vegetation_type:
         congform.errors.vegetation_type=T("Debe elegir un tipo de vegetación predominante")
 
 def controladorConglomerado():
-    congform = SQLFORM(db.Conglomerate_sample, formstyle='table3cols')
-    #site1form = SQLFORM(db.Site_sample)
-    #site2form = SQLFORM(db.Site_sample)
-    #site3form = SQLFORM(db.Site_sample)
-    #site4form = SQLFORM(db.Site_sample)
-    #controlform = SQLFORM(db.Site_sample)
-    if congform.process(onvalidation=additionalValidations).accepted:
-    #and site1form.process().accepted and site2form.process().accepted\
-    #and site3form.process().accepted and site4form.process().accepted\
-    #and controlform.process().accepted:
+    congform = SQLFORM(db.Conglomerate_sample,  _id='forma_conglomerado', formstyle='table3cols')
+    site1form = SQLFORM(db.Site_sample)
+    site2form = SQLFORM(db.Site_sample)
+    site3form = SQLFORM(db.Site_sample)
+    site4form = SQLFORM(db.Site_sample)
+    controlform = SQLFORM(db.Site_sample)
+    if congform.process(onvalidation=additionalValidations).accepted\
+    and site1form.process().accepted and site2form.process().accepted\
+    and site3form.process().accepted and site4form.process().accepted\
+    and controlform.process().accepted:
         response.flash = "Registro ingresado exitosamente"
-    return dict(congform=congform)#, site1form=site1form, site2form=site2form, site3form=site3form,\
-                #site4form=site4form, controlform=controlform)
+    return dict(congform=congform, site1form=site1form, site2form=site2form, site3form=site3form,\
+                site4form=site4form, controlform=controlform)
 
 #def check(form):
 #    if form.vars.b and not form.vars.c:
