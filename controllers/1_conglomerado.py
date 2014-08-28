0a75d30d7422d6917c606101a1b54bd712f18588
tereom <teresa.ortiz.mancera@gmail.com>
2014-08-28 16:29:48 -0500

    traducir nombres de ingles a español

--- controllers/1_conglomerado.py
+++ controllers/1_conglomerado.py
@@ -1,28 +1,28 @@
 # coding: utf8
 
 def additionalValidations(congform):
-    #If soil_use_type is not 'Vegetación' then is_perturbed=None
-    if not congform.vars.soil_use_type==T('Vegetación'):
-        congform.vars.vegetation_type=None
-        congform.vars.is_perturbed=None
-    #Checking if the vegetation_type control is filled when soil_use_type='Vegetación'
-    elif congform.vars.vegetation_type==None:
-        congform.errors.vegetation_type=T("Debe elegir un tipo de vegetación predominante")
+    #Si tipo_uso_suelo no es 'Vegetación' entonce perturbado=None
+    if not congform.vars.tipo_uso_suelo==T('Vegetación'):
+        congform.vars.vegetacion_tipo=None
+        congform.vars.perturbado=None
+    # Revisamos si se ingresó vegetacion_tipo cuando tipo_uso_suelo='Vegetación'
+    elif congform.vars.vegetacion_tipo==None:
+        congform.errors.vegetacion_tipo=T("Debe elegir un tipo de vegetación predominante")
 
 def controladorConglomerado():
-    congform = SQLFORM(db.Conglomerate_sample,  _id='forma_conglomerado')
-    #site1form = SQLFORM(db.Site_sample, _id='forma_sitio1')
-    #site2form = SQLFORM(db.Site_sample, _id='forma_sitio2')
-    #site3form = SQLFORM(db.Site_sample, _id='forma_sitio3')
-    #site4form = SQLFORM(db.Site_sample, _id='forma_sitio4')
+    congform = SQLFORM(db.Conglomerado_muestra,  _id='forma_conglomerado')
+    #sitio1form = SQLFORM(db.Site_sample, _id='forma_sitio1')
+    #sitio2form = SQLFORM(db.Site_sample, _id='forma_sitio2')
+    #sitio3form = SQLFORM(db.Site_sample, _id='forma_sitio3')
+    #sitio4form = SQLFORM(db.Site_sample, _id='forma_sitio4')
     #controlform = SQLFORM(db.Site_sample, _id='forma_control')
     if congform.process(onvalidation=additionalValidations).accepted:
-    #and site1form.process().accepted and site2form.process().accepted\
-    #and site3form.process().accepted and site4form.process().accepted\
+    #and sitio1form.process().accepted and sitio2form.process().accepted\
+    #and sitio3form.process().accepted and sitio4form.process().accepted\
     #and controlform.process().accepted:
         response.flash = "Registro ingresado exitosamente"
-    return dict(congform=congform)#, site1form=site1form, site2form=site2form, site3form=site3form,\
-                #site4form=site4form, controlform=controlform)
+    return dict(congform=congform)#, sitio1form=sitio1form, sitio2form=sitio2form, sitio3form=sitio3form,\
+                #sitio4form=sitio4form, controlform=controlform)
 
 #def check(form):
 #    if form.vars.b and not form.vars.c:
---
+++ models/1_conglomerado_m.py
@@ -0,0 +1,87 @@
+# coding: utf8
+
+## En esta sección se definen las tablas correspondientes a la pestaña de Conglomerado, es decir: Conglomerado_muestra, Sitio_muestra y Reference_image_site:
+## El campo de ID es automático en Web2py, por lo que no se incluye:
+
+db.define_table('Conglomerado_muestra',
+                Field('nombre','integer',label=T("Num. conglomerado"),\
+                        required='TRUE'),
+                Field('fecha_visita', 'date',label=T("Fecha de visita"),\
+                        required='TRUE'),
+                Field('tipo', 'reference Conglomerado_tipo_opcion',\
+                        label=T("Tipo de conglomerado"),required='TRUE'),
+                Field('estado', 'reference Conglomerado_estado_opcion',\
+                        label=T("Estado"),required='TRUE'),
+                Field('municipio', 'integer',label=T("Municipio"),required='TRUE'),
+                Field('predio','string',label=T("Predio"),required='TRUE'),
+                Field('tenencia', 'reference Conglomerado_tenencia_opcion',\
+                        label=T("Tenencia"), required='TRUE'),
+                Field('tipo_uso_suelo', 'reference Conglomerado_suelo_opcion',
+                        label=T("Tipo de uso de suelo"),required='TRUE'),
+                Field('vegetacion_tipo',\
+                        'reference Conglomerado_vegetacion_opcion',\
+                        label=T("Tipo de vegetación")),
+                Field('perturbado', 'boolean',label=T("Perturbado")),
+                Field('comentario', 'text',label=T("Observaciones")))
+
+db.Conglomerado_muestra.nombre.requires=IS_NOT_EMPTY()
+db.Conglomerado_muestra.fecha_visita.requires=IS_NOT_EMPTY()
+db.Conglomerado_muestra.tipo.requires=IS_IN_DB(db,db.Conglomerado_tipo_opcion.num_tipo,'%(nombre_tipo)s')
+db.Conglomerado_muestra.estado.requires=IS_IN_DB(db,db.Conglomerado_estado_opcion.num_estado,'%(nombre_estado)s')
+db.Conglomerado_muestra.municipio.requires=IS_NOT_EMPTY()
+db.Conglomerado_muestra.predio.requires=IS_NOT_EMPTY()
+db.Conglomerado_muestra.tenencia.requires=IS_IN_DB(db,db.Conglomerado_tenencia_opcion.num_tenencia,'%(nombre_tenencia)s')
+db.Conglomerado_muestra.tipo_uso_suelo.requires=IS_IN_DB(db,db.Conglomerado_suelo_opcion.num_suelo,'%(nombre_suelo)s')
+db.Conglomerado_muestra.vegetacion_tipo.requires=(IS_IN_DB(db,db.Conglomerado_vegetacion_opcion.num_vegetacion,'%(nombre_vegetacion)s') or None)
+
+db.define_table('Sitio_muestra',
+                Field('conglomerado_muestra_id','reference Conglomerado_muestra'),
+                Field('sitio_numero', 'reference Sitio_numero_opcion',\
+                        label=T("Número de sitio")),
+                Field('lat_grado','integer',label=T("grado"),required='TRUE'),
+                Field('lat_min','integer',label=T("minuto"),required='TRUE'),
+                Field('lat_seg','double',label=T("segundo"),required='TRUE'),
+                Field('lon_grado','integer',label=T("grado"),required='TRUE'),
+                Field('lon_min','integer',label=T("minuto"),required='TRUE'),
+                Field('lon_seg','double',label=T("segundo"),required='TRUE'),
+                Field('altitud','double',label=T("Altitud(m)"),required='TRUE'),
+                Field('gps_error','double',label=T("Error(m)"),required='TRUE'),
+                Field('elipsoide', 'reference Sitio_elipsoide_opcion',\
+                        label=T("Datum"),required='TRUE'), 
+                Field('evidencia', 'boolean',label=T("Evidencias")), 
+                Field('existe', 'boolean',label=T("Existe")))
+
+
+db.Sitio_muestra.sitio_numero.requires=IS_IN_DB(db,db.Sitio_numero_opcion.num_numero,'%(nombre_numero)s')
+db.Sitio_muestra.lat_grado.requires=IS_NOT_EMPTY()
+db.Sitio_muestra.lat_min.requires=IS_NOT_EMPTY()
+db.Sitio_muestra.lat_seg.requires=IS_NOT_EMPTY()
+db.Sitio_muestra.lon_grado.requires=IS_NOT_EMPTY()
+db.Sitio_muestra.lon_min.requires=IS_NOT_EMPTY()
+db.Sitio_muestra.lon_seg.requires=IS_NOT_EMPTY()
+db.Sitio_muestra.altitud.requires=IS_NOT_EMPTY()
+db.Sitio_muestra.gps_error.requires=IS_NOT_EMPTY()
+db.Sitio_muestra.elipsoide.requires=IS_IN_DB(db,db.Sitio_elipsoide_opcion.num_elipsoide,'%(nombre_elipsoide)s')
+
+
+
+# db.define_table('Reference_image_site',
+#                 Field('site_muestra_id','reference Sitio_muestra'),
+#                 Field('filenombre','text',label=T("Archivo")),
+#                 Field('oldfilenombre','text',label=T("Archivo")),
+#                 )
+
+
+
+########################################################################
+## Fields can be 'string','text','password','integer','double','boolean'
+##       'date','time','datetime','blob','upload', 'reference TABLENAME'
+## There is an implicit 'id integer autoincrement' field
+## Consult manual for more opcions, validators, etc.
+##
+## More API examples for controllers:
+##
+## >>> db.mytable.insert(myfield='value')
+## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
+## >>> for row in rows: print row.id, row.myfield
+#########################################################################
