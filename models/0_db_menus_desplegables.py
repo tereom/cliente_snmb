26d2b6db9b7deae21e49d9cc242a978b27ce569a
tereom <teresa.ortiz.mancera@gmail.com>
2014-08-28 13:00:30 -0500

    quitar bugs en conglomerado

--- models/0_db_menus_desplegables.py
+++ models/0_db_menus_desplegables.py
@@ -72,8 +72,8 @@ if db(db.Conglomerate_property_option.id>0).count() == 0:
 
 ######################################################################### Conglomerate_soil_option
 db.define_table('Conglomerate_soil_option',
-                Field('num_soil','integer', required='TRUE'),
-                Field('name_soil', 'text', required='TRUE'))
+                Field('num_soil','integer'),
+                Field('name_soil', 'text'))
 
 if db(db.Conglomerate_soil_option.id>0).count() == 0:
     db.Conglomerate_soil_option.insert(num_soil='1', name_soil='Agricultura de riego')
@@ -87,7 +87,7 @@ if db(db.Conglomerate_soil_option.id>0).count() == 0:
     db.Conglomerate_soil_option.insert(num_soil='9', name_soil='Minería a cielo abierto')
     db.Conglomerate_soil_option.insert(num_soil='10', name_soil='Jales mineros')
     db.Conglomerate_soil_option.insert(num_soil='11', name_soil='Incendios')
-    db.Conglomerate_soil_option.insert(num_soil='12', name_soil='Tipo de vegetación')
+    db.Conglomerate_soil_option.insert(num_soil='12', name_soil='Vegetación')
     db.Conglomerate_soil_option.insert(num_soil='13', name_soil='Otros')
 
 
--- models/1_conglomerado_model.py
+++ models/1_conglomerado_model.py
@@ -30,10 +30,9 @@ db.Conglomerate_sample.type_.requires=IS_IN_DB(db,db.Conglomerate_type_option.nu
 db.Conglomerate_sample.state_.requires=IS_IN_DB(db,db.Conglomerate_state_option.num_state,'%(name_state)s')
 db.Conglomerate_sample.municipio.requires=IS_NOT_EMPTY()
 db.Conglomerate_sample.predio.requires=IS_NOT_EMPTY()
-db.Conglomerate_sample.property_.requires==IS_IN_DB(db,db.Conglomerate_property_option.num_property,'%(name_property)s')
+db.Conglomerate_sample.property_.requires=IS_IN_DB(db,db.Conglomerate_property_option.num_property,'%(name_property)s')
 db.Conglomerate_sample.soil_use_type.requires=IS_IN_DB(db,db.Conglomerate_soil_option.num_soil,'%(name_soil)s')
 db.Conglomerate_sample.vegetation_type.requires=IS_IN_DB(db,db.Conglomerate_vegetation_option.num_vegetation,'%(name_vegetation)s')
-
 
 db.define_table('Site_sample',
                 Field('conglomerate_sample_id','reference Conglomerate_sample'),
