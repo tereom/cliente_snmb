a44930dd42607e641a0182e9b72051ac8b5d9492
tereom <teresa.ortiz.mancera@gmail.com>
2014-09-02 10:46:43 -0500

    avances en modelos

--- controllers/1_conglomerado.py
+++ controllers/1_conglomerado.py
@@ -1,6 +1,6 @@
 # coding: utf8
 
-def additionalValidations(congform):
+def validacionesAdicionales(congform):
     #Si uso_suelo_tipo no es 'Vegetación' entonce perturbado=None
     if not congform.vars.uso_suelo_tipo==T('Vegetación'):
         congform.vars.vegetacion_tipo=None
@@ -8,22 +8,6 @@ def additionalValidations(congform):
     # Revisamos si se ingresó vegetacion_tipo cuando uso_suelo_tipo='Vegetación'
     elif congform.vars.vegetacion_tipo==None:
         congform.errors.vegetacion_tipo=T("Debe elegir un tipo de vegetación predominante")
-
-# def controladorConglomerado():
-#     congform = SQLFORM(db.Conglomerado_muestra, _id='forma_conglomerado')
-#     sitio1form = SQLFORM(db.Sitio_muestra, _id='forma_sitio1')
-#     sitio2form = SQLFORM(db.Sitio_muestra, _id='forma_sitio2')
-#     sitio3form = SQLFORM(db.Sitio_muestra, _id='forma_sitio3')
-#     sitio4form = SQLFORM(db.Sitio_muestra, _id='forma_sitio4')
-#     controlform = SQLFORM(db.Sitio_muestra, _id='forma_control')
-#     if congform.process(onvalidation=additionalValidations).accepted\
-#     and sitio1form.process().accepted and sitio2form.process().accepted\
-#     and sitio3form.process().accepted and sitio4form.process().accepted\
-#     and controlform.process().accepted:
-#         response.flash = "Registro ingresado exitosamente"
-#     return dict(congform=congform, sitio1form=sitio1form, sitio2form=sitio2form, sitio3form=sitio3form,\
-#                 sitio4form=sitio4form, controlform=controlform)
-
 
 def controladorConglomerado():
     congform = SQLFORM(db.Conglomerado_muestra, _id='forma_conglomerado')
@@ -37,13 +21,16 @@ def controladorConglomerado():
         _id='forma_sitio4', table_name='conjunta_sitio_imagen')
     controlform = SQLFORM.factory(db.Sitio_muestra, db.Imagen_referencia_sitio,\
      _id='forma_control', table_name='conjunta_sitio_imagen')
-    if congform.process(onvalidation=additionalValidations).accepted\
+    if congform.process(onvalidation=validacionesAdicionales).accepted\
     and sitio1form.process().accepted and sitio2form.process().accepted\
     and sitio3form.process().accepted and sitio4form.process().accepted\
     and controlform.process().accepted:
         response.flash = "Registro ingresado exitosamente"
     return dict(congform=congform, sitio1form=sitio1form, sitio2form=sitio2form, sitio3form=sitio3form,\
                 sitio4form=sitio4form, controlform=controlform)
+
+
+
 
 
 #def check(form):
