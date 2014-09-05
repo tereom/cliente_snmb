a74b8e78c39f7196735a3f54d04462896dd80336
fpardourrutia <firedragon_fernando@hotmail.com>
2014-09-05 18:09:11 -0500

    Ya guarda en base de datos todos los sitios

--- controllers/1_conglomerado.py
+++ controllers/1_conglomerado.py
@@ -106,91 +106,91 @@ def index():
 # 		##que no es muy cómodo incluir una imagen default si el sitio no existe, y luego borrarla
 # 		##en el controlador.
 # 	
-# 		###############################
-# 		#Campos del sitio 2
-# 		###############################
-# 		
-# 		##Los campos condicionales a existe se definirán como obligatorios, sin embargo, si el
-# 		##sitio no existe, se esconderán y se les asignará un valor arbitrario para que pasen
-# 		##la validación de la forma. Posteriormente aquí en el controlador se borrarán dichos valores.
-# 		##Esto porque en Web2py no es sencillo definir:
-# 		##	"si este campo está marcado, entonces este otro es obligatorio, si no, ni aparece".
-# 	
-# 		Field('existe_2', 'boolean',label=T("Existe")),
-# 		Field('lat_grado_2','integer',label=T("grado"), requires=IS_NOT_EMPTY()),
-# 		Field('lat_min_2','integer',label=T("minuto"), requires=IS_NOT_EMPTY()),
-# 		Field('lat_seg_2','double',label=T("segundo"), requires=IS_NOT_EMPTY()),
-# 		Field('lon_grado_2','integer',label=T("grado"), requires=IS_NOT_EMPTY()),
-# 		Field('lon_min_2','integer',label=T("minuto"), requires=IS_NOT_EMPTY()),
-# 		Field('lon_seg_2','double',label=T("segundo"), requires=IS_NOT_EMPTY()),
-#     	Field('altitud_2','double',label=T("Altitud(m)"), requires=IS_NOT_EMPTY()),
-#     	Field('gps_error_2','double',label=T("Error(m)"), requires=IS_NOT_EMPTY()),
-# 		Field('elipsoide_2', label=T("Datum"),
-# 			requires=IS_IN_DB(db(db.Cat_elipsoide_sitio.nombre!='No aplica'),
-#         	db.Cat_elipsoide_sitio.id,'%(nombre)s')),
-#     	Field('evidencia_2', 'boolean',label=XML("Evidencia <br/> anterior")),
-#     	
+		###############################
+		#Campos del sitio 2
+		###############################
+		
+		##Los campos condicionales a existe se definirán como obligatorios, sin embargo, si el
+		##sitio no existe, se esconderán y se les asignará un valor arbitrario para que pasen
+		##la validación de la forma. Posteriormente aquí en el controlador se borrarán dichos valores.
+		##Esto porque en Web2py no es sencillo definir:
+		##	"si este campo está marcado, entonces este otro es obligatorio, si no, ni aparece".
+	
+		Field('existe_2', 'boolean',label=T("Existe")),
+		Field('lat_grado_2','integer',label=T("grado"), requires=IS_NOT_EMPTY()),
+		Field('lat_min_2','integer',label=T("minuto"), requires=IS_NOT_EMPTY()),
+		Field('lat_seg_2','double',label=T("segundo"), requires=IS_NOT_EMPTY()),
+		Field('lon_grado_2','integer',label=T("grado"), requires=IS_NOT_EMPTY()),
+		Field('lon_min_2','integer',label=T("minuto"), requires=IS_NOT_EMPTY()),
+		Field('lon_seg_2','double',label=T("segundo"), requires=IS_NOT_EMPTY()),
+    	Field('altitud_2','double',label=T("Altitud(m)"), requires=IS_NOT_EMPTY()),
+    	Field('gps_error_2','double',label=T("Error(m)"), requires=IS_NOT_EMPTY()),
+		Field('elipsoide_2', label=T("Datum"),
+			requires=IS_IN_DB(db(db.Cat_elipsoide_sitio.nombre!='No aplica'),
+        	db.Cat_elipsoide_sitio.id,'%(nombre)s')),
+    	Field('evidencia_2', 'boolean',label=XML("Evidencia <br/> anterior")),
+    	
 #     	###########Imagen############
 # 		Field('archivo_nombre_original_2', 'upload', label=T("Fotografía")),
 # 
-# 		###############################
-# 		#Campos del sitio 3
-# 		###############################
-# 	
-# 		Field('existe_3', 'boolean',label=T("Existe")),
-# 		Field('lat_grado_3','integer',label=T("grado"), requires=IS_NOT_EMPTY()),
-# 		Field('lat_min_3','integer',label=T("minuto"), requires=IS_NOT_EMPTY()),
-# 		Field('lat_seg_3','double',label=T("segundo"), requires=IS_NOT_EMPTY()),
-# 		Field('lon_grado_3','integer',label=T("grado"), requires=IS_NOT_EMPTY()),
-# 		Field('lon_min_3','integer',label=T("minuto"), requires=IS_NOT_EMPTY()),
-# 		Field('lon_seg_3','double',label=T("segundo"), requires=IS_NOT_EMPTY()),
-#     	Field('altitud_3','double',label=T("Altitud(m)"), requires=IS_NOT_EMPTY()),
-#     	Field('gps_error_3','double',label=T("Error(m)"), requires=IS_NOT_EMPTY()),
-# 		Field('elipsoide_3', label=T("Datum"),
-# 			requires=IS_IN_DB(db(db.Cat_elipsoide_sitio.nombre!='No aplica'),
-#         	db.Cat_elipsoide_sitio.id,'%(nombre)s')),
-#     	Field('evidencia_3', 'boolean',label=XML("Evidencia <br/> anterior")),
+		###############################
+		#Campos del sitio 3
+		###############################
+	
+		Field('existe_3', 'boolean',label=T("Existe")),
+		Field('lat_grado_3','integer',label=T("grado"), requires=IS_NOT_EMPTY()),
+		Field('lat_min_3','integer',label=T("minuto"), requires=IS_NOT_EMPTY()),
+		Field('lat_seg_3','double',label=T("segundo"), requires=IS_NOT_EMPTY()),
+		Field('lon_grado_3','integer',label=T("grado"), requires=IS_NOT_EMPTY()),
+		Field('lon_min_3','integer',label=T("minuto"), requires=IS_NOT_EMPTY()),
+		Field('lon_seg_3','double',label=T("segundo"), requires=IS_NOT_EMPTY()),
+    	Field('altitud_3','double',label=T("Altitud(m)"), requires=IS_NOT_EMPTY()),
+    	Field('gps_error_3','double',label=T("Error(m)"), requires=IS_NOT_EMPTY()),
+		Field('elipsoide_3', label=T("Datum"),
+			requires=IS_IN_DB(db(db.Cat_elipsoide_sitio.nombre!='No aplica'),
+        	db.Cat_elipsoide_sitio.id,'%(nombre)s')),
+    	Field('evidencia_3', 'boolean',label=XML("Evidencia <br/> anterior")),
 #     	
 #     	###########Imagen############
 # 		Field('archivo_nombre_original_3', 'upload', label=T("Fotografía")),
 # 
-# 		###############################
-# 		#Campos del sitio 4
-# 		###############################
-# 	
-# 		Field('existe_4', 'boolean',label=T("Existe")),
-# 		Field('lat_grado_4','integer',label=T("grado"), requires=IS_NOT_EMPTY()),
-# 		Field('lat_min_4','integer',label=T("minuto"), requires=IS_NOT_EMPTY()),
-# 		Field('lat_seg_4','double',label=T("segundo"), requires=IS_NOT_EMPTY()),
-# 		Field('lon_grado_4','integer',label=T("grado"), requires=IS_NOT_EMPTY()),
-# 		Field('lon_min_4','integer',label=T("minuto"), requires=IS_NOT_EMPTY()),
-# 		Field('lon_seg_4','double',label=T("segundo"), requires=IS_NOT_EMPTY()),
-#     	Field('altitud_4','double',label=T("Altitud(m)"), requires=IS_NOT_EMPTY()),
-#     	Field('gps_error_4','double',label=T("Error(m)"), requires=IS_NOT_EMPTY()),
-# 		Field('elipsoide_4', label=T("Datum"),
-# 			requires=IS_IN_DB(db(db.Cat_elipsoide_sitio.nombre!='No aplica'),
-#         	db.Cat_elipsoide_sitio.id,'%(nombre)s')),
-#     	Field('evidencia_4', 'boolean',label=XML("Evidencia <br/> anterior")),
+		###############################
+		#Campos del sitio 4
+		###############################
+	
+		Field('existe_4', 'boolean',label=T("Existe")),
+		Field('lat_grado_4','integer',label=T("grado"), requires=IS_NOT_EMPTY()),
+		Field('lat_min_4','integer',label=T("minuto"), requires=IS_NOT_EMPTY()),
+		Field('lat_seg_4','double',label=T("segundo"), requires=IS_NOT_EMPTY()),
+		Field('lon_grado_4','integer',label=T("grado"), requires=IS_NOT_EMPTY()),
+		Field('lon_min_4','integer',label=T("minuto"), requires=IS_NOT_EMPTY()),
+		Field('lon_seg_4','double',label=T("segundo"), requires=IS_NOT_EMPTY()),
+    	Field('altitud_4','double',label=T("Altitud(m)"), requires=IS_NOT_EMPTY()),
+    	Field('gps_error_4','double',label=T("Error(m)"), requires=IS_NOT_EMPTY()),
+		Field('elipsoide_4', label=T("Datum"),
+			requires=IS_IN_DB(db(db.Cat_elipsoide_sitio.nombre!='No aplica'),
+        	db.Cat_elipsoide_sitio.id,'%(nombre)s')),
+    	Field('evidencia_4', 'boolean',label=XML("Evidencia <br/> anterior")),
 #     	
 #     	###########Imagen############
 # 		Field('archivo_nombre_original_4', 'upload', label=T("Fotografía")),
 # 
-# 		###############################
-# 		#Campos del punto de control
-# 		###############################
-# 
-# 		Field('lat_grado_c','integer',label=T("grado"), requires=IS_NOT_EMPTY()),
-# 		Field('lat_min_c','integer',label=T("minuto"), requires=IS_NOT_EMPTY()),
-# 		Field('lat_seg_c','double',label=T("segundo"), requires=IS_NOT_EMPTY()),
-# 		Field('lon_grado_c','integer',label=T("grado"), requires=IS_NOT_EMPTY()),
-# 		Field('lon_min_c','integer',label=T("minuto"), requires=IS_NOT_EMPTY()),
-# 		Field('lon_seg_c','double',label=T("segundo"), requires=IS_NOT_EMPTY()),
-#     	Field('altitud_c','double',label=T("Altitud(m)"), requires=IS_NOT_EMPTY()),
-#     	Field('gps_error_c','double',label=T("Error(m)"), requires=IS_NOT_EMPTY()),
-# 		Field('elipsoide_c', label=T("Datum"),
-# 			requires=IS_IN_DB(db(db.Cat_elipsoide_sitio.nombre!='No aplica'),
-#         	db.Cat_elipsoide_sitio.id,'%(nombre)s')),
-#     	Field('evidencia_c', 'boolean',label=XML("Evidencia <br/> anterior")),
+		###############################
+		#Campos del punto de control
+		###############################
+
+		Field('lat_grado_c','integer',label=T("grado"), requires=IS_NOT_EMPTY()),
+		Field('lat_min_c','integer',label=T("minuto"), requires=IS_NOT_EMPTY()),
+		Field('lat_seg_c','double',label=T("segundo"), requires=IS_NOT_EMPTY()),
+		Field('lon_grado_c','integer',label=T("grado"), requires=IS_NOT_EMPTY()),
+		Field('lon_min_c','integer',label=T("minuto"), requires=IS_NOT_EMPTY()),
+		Field('lon_seg_c','double',label=T("segundo"), requires=IS_NOT_EMPTY()),
+    	Field('altitud_c','double',label=T("Altitud(m)"), requires=IS_NOT_EMPTY()),
+    	Field('gps_error_c','double',label=T("Error(m)"), requires=IS_NOT_EMPTY()),
+		Field('elipsoide_c', label=T("Datum"),
+			requires=IS_IN_DB(db(db.Cat_elipsoide_sitio.nombre!='No aplica'),
+        	db.Cat_elipsoide_sitio.id,'%(nombre)s')),
+    	Field('evidencia_c', 'boolean',label=XML("Evidencia <br/> anterior")),
 #     	
 #     	###########Imagen############
 #     	Field('archivo_nombre_original_c', 'upload', label=T("Fotografía")),
@@ -246,6 +246,7 @@ def index():
 		################Procesando los datos del sitio 1##############################
 
 		#Agregando los datos que no se pidieron al usuario:
+		
 		formaSitio1 = {}
 		formaSitio1['conglomerado_muestra_id']=conglomeradoInsertado
 		formaSitio1['sitio_numero']=1
@@ -261,20 +262,143 @@ def index():
 		formaSitio1['altitud']=forma.vars['altitud_1']
 		formaSitio1['gps_error']=forma.vars['gps_error_1']
 		formaSitio1['elipsoide']=forma.vars['elipsoide_1']
-		formaSitio1['evidencia']=forma.vars['evidencia_1']
 		
+		#Si hay evidencia, entonces True se guarda en la base de datos, en caso contrario,
+		#se tiene que guardar manualmente False, pues si no, Web2py guarda Null.
+		if bool(forma.vars['evidencia_1']):
+			formaSitio1['evidencia']=forma.vars['evidencia_1']
+		else:
+			formaSitio1['evidencia']=False
 		#Insertando en la base de datos:
 		db.Sitio_muestra.insert(**formaSitio1)
 
+		################Procesando los datos del sitio 2##############################
+
+		#Agregando los datos que no se pidieron al usuario:
 		
-        #    variables['perturbado']=None
-        #    variables['vegetacion_tipo']=opcion_vegetacion.id
-        # elif opcion_suelo.id<variables['uso_suelo_tipo']:
-        #     variables['perturbado']=None
-        #     variables['vegetacion_tipo']=opcion_vegetacion.id
-        #else:
-        #    variables['vegetacion_tipo']=congForm.vars.vegetacion_tipo_aux
-        #db.Conglomerado_muestra.insert(**variables)
-        #response.flash = "Registro ingresado exitosamente"
+		formaSitio2 = {}
+		formaSitio2['conglomerado_muestra_id']=conglomeradoInsertado
+		formaSitio2['sitio_numero']=2
+		
+		#Si existe el sitio 2:
+		if bool(forma.vars['existe_2']):
+
+			#Agregando los datos extraídos de la forma:
+			formaSitio2['existe']=forma.vars['existe_2']
+			formaSitio2['lat_grado']=forma.vars['lat_grado_2']
+			formaSitio2['lat_min']=forma.vars['lat_min_2']
+			formaSitio2['lat_seg']=forma.vars['lat_seg_2']
+			formaSitio2['lon_grado']=forma.vars['lon_grado_2']
+			formaSitio2['lon_min']=forma.vars['lon_min_2']
+			formaSitio2['lon_seg']=forma.vars['lon_seg_2']
+			formaSitio2['altitud']=forma.vars['altitud_2']
+			formaSitio2['gps_error']=forma.vars['gps_error_2']
+			formaSitio2['elipsoide']=forma.vars['elipsoide_2']
+			
+			if bool(forma.vars['evidencia_2']):
+				formaSitio2['evidencia']=forma.vars['evidencia_2']
+			else:
+				formaSitio2['evidencia']=False
+				
+		else:
+			formaSitio2['existe']=False
+		
+		#Insertando en la base de datos:
+		db.Sitio_muestra.insert(**formaSitio2)
+		
+		################Procesando los datos del sitio3##############################
+
+		#Agregando los datos que no se pidieron al usuario:
+		
+		formaSitio3 = {}
+		formaSitio3['conglomerado_muestra_id']=conglomeradoInsertado
+		formaSitio3['sitio_numero']=3
+		
+		#Si existe el sitio 3:
+		if bool(forma.vars['existe_3']):
+
+			#Agregando los datos extraídos de la forma:
+			formaSitio3['existe']=forma.vars['existe_3']
+			formaSitio3['lat_grado']=forma.vars['lat_grado_3']
+			formaSitio3['lat_min']=forma.vars['lat_min_3']
+			formaSitio3['lat_seg']=forma.vars['lat_seg_3']
+			formaSitio3['lon_grado']=forma.vars['lon_grado_3']
+			formaSitio3['lon_min']=forma.vars['lon_min_3']
+			formaSitio3['lon_seg']=forma.vars['lon_seg_3']
+			formaSitio3['altitud']=forma.vars['altitud_3']
+			formaSitio3['gps_error']=forma.vars['gps_error_3']
+			formaSitio3['elipsoide']=forma.vars['elipsoide_3']
+			
+			if bool(forma.vars['evidencia_3']):
+				formaSitio3['evidencia']=forma.vars['evidencia_3']
+			else:
+				formaSitio3['evidencia']=False
+				
+		else:
+			formaSitio3['existe']=False
+		
+		#Insertando en la base de datos:
+		db.Sitio_muestra.insert(**formaSitio3)
+
+		################Procesando los datos del sitio4##############################
+
+		#Agregando los datos que no se pidieron al usuario:
+		
+		formaSitio4 = {}
+		formaSitio4['conglomerado_muestra_id']=conglomeradoInsertado
+		formaSitio4['sitio_numero']=4
+		
+		#Si existe el sitio 4:
+		if bool(forma.vars['existe_4']):
+
+			#Agregando los datos extraídos de la forma:
+			formaSitio4['existe']=forma.vars['existe_4']
+			formaSitio4['lat_grado']=forma.vars['lat_grado_4']
+			formaSitio4['lat_min']=forma.vars['lat_min_4']
+			formaSitio4['lat_seg']=forma.vars['lat_seg_4']
+			formaSitio4['lon_grado']=forma.vars['lon_grado_4']
+			formaSitio4['lon_min']=forma.vars['lon_min_4']
+			formaSitio4['lon_seg']=forma.vars['lon_seg_4']
+			formaSitio4['altitud']=forma.vars['altitud_4']
+			formaSitio4['gps_error']=forma.vars['gps_error_4']
+			formaSitio4['elipsoide']=forma.vars['elipsoide_4']
+			
+			if bool(forma.vars['evidencia_4']):
+				formaSitio4['evidencia']=forma.vars['evidencia_4']
+			else:
+				formaSitio4['evidencia']=False
+				
+		else:
+			formaSitio4['existe']=False
+		
+		#Insertando en la base de datos:
+		db.Sitio_muestra.insert(**formaSitio4)
+
+		################Procesando los datos del punto de control#########################
+
+		#Agregando los datos que no se pidieron al usuario:
+		
+		formaSitioC = {}
+		formaSitioC['conglomerado_muestra_id']=conglomeradoInsertado
+		formaSitioC['sitio_numero']=5
+		formaSitioC['existe']=True
+		
+		#Leyendo los datos del formulario:
+		formaSitioC['lat_grado']=forma.vars['lat_grado_c']
+		formaSitioC['lat_min']=forma.vars['lat_min_c']
+		formaSitioC['lat_seg']=forma.vars['lat_seg_c']
+		formaSitioC['lon_grado']=forma.vars['lon_grado_c']
+		formaSitioC['lon_min']=forma.vars['lon_min_c']
+		formaSitioC['lon_seg']=forma.vars['lon_seg_c']
+		formaSitioC['altitud']=forma.vars['altitud_c']
+		formaSitioC['gps_error']=forma.vars['gps_error_c']
+		formaSitioC['elipsoide']=forma.vars['elipsoide_c']
+		
+		if bool(forma.vars['evidencia_c']):
+			formaSitioC['evidencia']=forma.vars['evidencia_c']
+		else:
+			formaSitioC['evidencia']=False
+		#Insertando en la base de datos:
+		db.Sitio_muestra.insert(**formaSitioC)
 
 	return dict(forma=forma)
