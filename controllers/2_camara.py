471df5b84cfa676316c3a2eba5c2840c3cc00149
tereom <teresa.ortiz.mancera@gmail.com>
2014-09-12 18:41:17 -0500

    avances pestaña 2

--- controllers/2_camara.py
+++ controllers/2_camara.py
@@ -1,62 +1,61 @@
+
+
 # coding: utf8
 def index(): 
 
-	Campos_pestana_2 = [
-	
-	# campos Camara
-	# Posteriormente habrá dropdowns en cascada para los campos de conglomerado y sitio,
-	# para abordar el caso en el que los sitios de un conglomerado puedan no existir.
-	
-	#Datos para localizar un sitio único y asociarle la cámara a éste.
-	#Estos datos deben ser una llave del sitio.
-	    Field('conglomerado_muestra_id', label=T("Conglomerado"),
-	    	requires=IS_IN_DB(db,db.Conglomerado_muestra.id,'%(nombre)s')),
-	    Field('sitio_numero',label=T("Sitio"),
-    		requires=IS_IN_DB(db,db.Cat_numero_sitio.id,'%(nombre)s')),
+    Campos_pestana_2 = [
+        # campos Camara
+    # Posteriormente habrá dropdowns en cascada para los campos de conglomerado y sitio,
+    # para abordar el caso en el que los sitios de un conglomerado puedan no existir.
     
-    #Datos de la cámara
-        Field('distancia_centro','double', label=T("Distancia al centro del sitio (m)"),
-    		requires=IS_NOT_EMPTY()),    		
-    	Field('fecha_inicio', 'date',label=T("Fecha de colocación"),requires=IS_NOT_EMPTY()),
-    	Field('fecha_termino', 'date',label=T("Fecha de levantamiento"),requires=IS_NOT_EMPTY()),	
-    	Field('hora_inicio','time',label=T("Hora inicio"),requires=IS_NOT_EMPTY()),
-    	Field('hora_termino', 'time',label=T("Hora término"),requires=IS_NOT_EMPTY()),
-    	Field('llovio', 'boolean',label=T("Llovió durante el muestreo")),
-    	
-    	Field('lat_grado','integer',label=T("grado"),requires=IS_NOT_EMPTY()),
-    	Field('lat_min','integer',label=T("minuto"),requires=IS_NOT_EMPTY()),
-    	Field('lat_seg','double',label=T("segundo"),requires=IS_NOT_EMPTY()),
-    	Field('lon_grado','integer',label=T("grado"),requires=IS_NOT_EMPTY()),
-    	Field('lon_min','integer',label=T("minuto"),requires=IS_NOT_EMPTY()),
-    	Field('lon_seg','double',label=T("segundo"),requires=IS_NOT_EMPTY()),
-    	Field('altitud','double',label=T("Altitud(m)"),requires=IS_NOT_EMPTY()),
-    	Field('gps_error','double',label=T("Error(m)"),requires=IS_NOT_EMPTY()),
-    	Field('elipsoide',label=T("Datum"),
-    		requires=IS_IN_DB(db,db.Cat_elipsoide_sitio.id,'%(nombre)s')),    		
+    #Datos para localizar un sitio único y asociarle la cámara a éste.
+    #Estos datos deben ser una llave del sitio.
+        #SELECT(_name='conglomerado_muestra_id',
+        #    requires=IS_IN_DB(db,db.Conglomerado_muestra.id,'%(nombre)s')),
+        SELECT(_name='sitio_numero',
+            requires=IS_IN_DB(db,db.Cat_numero_sitio.id,'%(nombre)s')),
+    
+    # #Datos de la cámara
+    #     INPUT(_name='distancia_centro',_type='double', 
+    #         requires=IS_NOT_EMPTY()),           
+    #     INPUT(_name='fecha_inicio',_type='date',requires=IS_NOT_EMPTY()),
+    #     INPUT(_name='fecha_termino',_type='date',requires=IS_NOT_EMPTY()),    
+    #     INPUT(_name='hora_inicio',_type='time',requires=IS_NOT_EMPTY()),
+    #     INPUT(_name='hora_termino',_type='time',requires=IS_NOT_EMPTY()),
+    #     INPUT(_name='llovio',_type='boolean'),
+        
+    #     INPUT(_name='lat_grado',_type='integer',requires=IS_NOT_EMPTY()),
+    #     INPUT(_name='lat_min',_type='integer',requires=IS_NOT_EMPTY()),
+    #     INPUT(_name='lat_seg',_type='double',requires=IS_NOT_EMPTY()),
+    #     INPUT(_name='lon_grado',_type='integer',requires=IS_NOT_EMPTY()),
+    #     INPUT(_name='lon_min',_type='integer',requires=IS_NOT_EMPTY()),
+    #     INPUT(_name='lon_seg',_type='double',requires=IS_NOT_EMPTY()),
+    #     INPUT(_name='altitud',_type='double',requires=IS_NOT_EMPTY()),
+    #     INPUT(_name='gps_error',_type='double',requires=IS_NOT_EMPTY()),
+    #     SELECT(_name='elipsoide',
+    #         requires=IS_IN_DB(db,db.Cat_elipsoide_sitio.id,'%(nombre)s')),          
 
-    	Field('nombre', label=T("Código cámara"),
-	    	requires=IS_IN_DB(db,db.Cat_nombre_camara.id,'%(nombre)s')),
-	    Field('resolucion', label=T("Resolución"),
-    		requires=IS_IN_DB(db,db.Cat_resolucion_camara.id,'%(nombre)s')),
-    	Field('sensibilidad', label=T("Sensibilidad"),
-    		requires=IS_IN_DB(db,db.Cat_sensibilidad_camara.id,'%(nombre)s')),
-    	Field('comentario', 'text',label=T("Observaciones")),	
+    #     SELECT(_name='nombre',
+    #         requires=IS_IN_DB(db,db.Cat_nombre_camara.id,'%(nombre)s')),
+    #     SELECT(_name='resolucion',
+    #         requires=IS_IN_DB(db,db.Cat_resolucion_camara.id,'%(nombre)s')),
+    #     SELECT(_name='sensibilidad',
+    #         requires=IS_IN_DB(db,db.Cat_sensibilidad_camara.id,'%(nombre)s')),
 
-     	###########Imagen############
- 		Field('imagen_camara', 'upload', label=T("Fotografía"), uploadfolder="naruto",
- 		requires=IS_NOT_EMPTY())
-	]
+        TEXTAREA(_name='comentario',_type='text')    
 
-	forma=SQLFORM.factory(*Campos_pestana_2,table_name='tabla')
+        ###########Imagen############
+        #INPUT(_name='imagen_camara',_type='file',requires=IS_NOT_EMPTY())
+    ]
 
-	if forma.validate():
-	'''
-		formaCamara=db.Camara._filter_fields(forma.vars)
-		camaraInsertado = db.Camara.insert(**formaCamara)
-		formaCamara['sitio_muestra_id']=camaraInsertado
+    forma = FORM(*Campos_pestana_2)
 
-		formaImagen = {}
-		formaImage['camara_id']=camaraInsertado
-	'''
+    if forma.accepts(request.vars,formname='prueba'):
+        response.flash = 'Thanks! The form has been submitted.'
+        formaCamara = db.Camara._filter_fields(request.vars)
+        camaraInsertada = db.Camara.insert(**formaCamara)
+        
+    elif forma.errors:
+       response.flash = 'Please correct the error(s).'
 
-	return dict()
+    return dict()
