5755fa380f280e9ffe0245c30999c1798f188e4a
Maria Ortiz <teresa.ortiz.mancera@gmail.com>
2014-10-10 18:22:51 -0500

    Controlador puntos carbono

--- models/10_carbono_m.py
+++ models/10_carbono_m.py
@@ -10,7 +10,7 @@
 Campos_Transecto_ramas = [
 
     Field('sitio_muestra_id','reference Sitio_muestra',required=True),
-    Field('transecto_ramas_direccion','string',required='True'),
+    Field('transecto_ramas_direccion','string',required=True),
     Field('pendiente','integer',required=True),
     Field('abundancia_1h','integer',required=True),
     Field('abundancia_10h','integer',required=True),
@@ -39,8 +39,8 @@ Campos_Rama_1000h = [
 
 Campos_Punto_carbono = [
 
-    Field('sitio_muestra_id','reference Sitio_muestra',required=True), 
-    Field('transecto_ramas_direccion','string',required='True'),
+    Field('sitio_muestra_id','reference Sitio_muestra',required=True),
+    Field('transecto_direccion','string',required=True),
     Field('transecto_distancia','integer',required=True),
 
     #Se insertará a partir de un catálogo
