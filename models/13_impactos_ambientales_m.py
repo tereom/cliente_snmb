8ef103d570ddee6b26e1a808b3aa8dc4d0a428f5
Maria Ortiz <teresa.ortiz.mancera@gmail.com>
2014-10-21 15:51:30 -0500

    Revisón modelo Pestaña ambientales

--- models/13_impactos_ambientales_m.py
+++ models/13_impactos_ambientales_m.py
@@ -29,7 +29,7 @@ db.define_table('Impacto_actual',*Campos_Impacto_actual,
 
 Campos_Plaga = [
 
-    Field('impacto_actual_id','reference Impacto_actual',required=True),
+    Field('conglomerado_muestra_id','reference Conglomerado_muestra',required=True),
     # agente se insertará a partir de un catálogo
     Field('agente','string',required=True),
     Field('nombre_comun','string'),
@@ -62,12 +62,14 @@ db.define_table('Archivo_plaga',*Campos_Archivo_plaga,
 ##########################################################################
 
 Campos_Incendio = [
-    Field('impacto_actual_id','reference Impacto_actual',required=True),
+    Field('conglomerado_muestra_id','reference Conglomerado_muestra',required=True),
     Field('es_anio_actual','boolean',required=True),
+    Field('hay_evidencia','boolean',required=True)
     Field('tipo','string',required=True),
     Field('prop_afectacion_herbacea','string',required=True),
     Field('prop_afectacion_arbustiva','string',required=True),
     Field('prop_afectacion_arborea','string',required=True),
+    Field('copa_quemada','string',required=True)
     Field('hay_evidencia_recuperacion','boolean',required=True)
     ]
 
