b9ae72c9f0d0fdc7d92cc064608933a215ecb6f6
fpardourrutia <fpardo@iMac-de-sst.local>
2014-12-05 16:15:03 -0600

    Validación selección de fauna

--- controllers/08_revision.py
+++ controllers/08_revision.py
@@ -16,67 +16,67 @@ def obtenerFotografia():
     #Creando la pantalla de revisión de fotografía, considerando el caso en el
     #que se seleccione la opción vacía:
 
-    #try:
+    try:
 
-    revisionHTML = "<form id='forma_shadow'><input type='hidden' name='id_foto' value='" +\
-        str(datosFoto.id) + "'/><center><img src='/cliente_web2py/08_revision/download/"+datosFoto.archivo+\
-        "' alt='Error al cargar la fotografía' style='width:800px;height:600px;'/></center>"+\
-        "<hr/><div><div style='float:left;padding-right:60px;'><label for='tabla_con_fauna_evidente' "+\
-        "style='float:left;padding-right:20px;'>Con fauna evidente</label><input type='radio' "+\
-        "name='fauna_evidente' value='encontrada' id='tabla_con_fauna_evidente'"
+        revisionHTML = "<form id='forma_shadow'><input type='hidden' name='id_foto' value='" +\
+            str(datosFoto.id) + "'/><center><img src='/cliente_web2py/08_revision/download/"+datosFoto.archivo+\
+            "' alt='Error al cargar la fotografía' style='width:800px;height:600px;'/></center>"+\
+            "<hr/><div><div style='float:left;padding-right:60px;'><label for='tabla_con_fauna_evidente' "+\
+            "style='float:left;padding-right:20px;'>Con fauna evidente</label><input type='radio' "+\
+            "name='fauna_evidente' value='encontrada' id='tabla_con_fauna_evidente'"
 
-#Si el campo de presencia de la foto elegida es True, entonces la casilla "fauna evidente" aparece marcada.
+        #Si el campo de presencia de la foto elegida es True, entonces la casilla "fauna evidente" aparece marcada.
 
-    if datosFoto.presencia:
+        if datosFoto.presencia:
 
-        revisionHTML += " checked='true'/>"
+            revisionHTML += " checked='true'/>"
 
-    else:
+        else:
 
-        revisionHTML += "/>"
+            revisionHTML += "/>"
 
-    revisionHTML += "</div><div style='float:left;'><label for='tabla_sin_fauna_evidente' "+\
-        "style='float:left;padding-right:20px;'>Sin fauna evidente</label><input type='radio' "+\
-        "name='fauna_evidente' value='no_encontrada' id='tabla_sin_fauna_evidente'"
-    
-    #Si el campo de presencia de la foto elegida es False, entonces la casilla "Sin fauna evidente" aparece marcada.
-    #Hay que revisar que sea igual a false, porque podría ser None.
-    if datosFoto.presencia==False:
+        revisionHTML += "</div><div style='float:left;'><label for='tabla_sin_fauna_evidente' "+\
+            "style='float:left;padding-right:20px;'>Sin fauna evidente</label><input type='radio' "+\
+            "name='fauna_evidente' value='no_encontrada' id='tabla_sin_fauna_evidente'"
+        
+        #Si el campo de presencia de la foto elegida es False, entonces la casilla "Sin fauna evidente" aparece marcada.
+        #Hay que revisar que sea igual a false, porque podría ser None.
+        if datosFoto.presencia==False:
 
-        revisionHTML += " checked='true'/>"
+            revisionHTML += " checked='true'/>"
 
-    else:
+        else:
 
-        revisionHTML += "/>"
+            revisionHTML += "/>"
 
-    revisionHTML += "</div></div><div style='clear:both;'></div><br/><div>"+\
-        "<label for='tabla_nombre_comun' style='float:left;padding-right:49px;'>"+\
-        "Nombre común:</label><input type='text' name='nombre_comun' "+\
-        "id='tabla_nombre_comun' value='"
+        revisionHTML += "</div></div><div style='clear:both;'></div><br/><div>"+\
+            "<label for='tabla_nombre_comun' style='float:left;padding-right:49px;'>"+\
+            "Nombre común:</label><input type='text' name='nombre_comun' class='string' "+\
+            "id='tabla_nombre_comun' value='"
 
-    #Si hay nombre común, éste aparece en la casilla para ingresar el texto.
-    if datosFoto.nombre_comun:
+        #Si hay nombre común, éste aparece en la casilla para ingresar el texto.
+        if datosFoto.nombre_comun:
 
-        revisionHTML += datosFoto.nombre_comun
+            revisionHTML += datosFoto.nombre_comun
 
-    revisionHTML += "'/></div><br/><div><label for='tabla_nombre_cientifico' "+\
-        "style='float:left;padding-right:37px;'>Nombre científico:</label>"+\
-        "<input type='text' name='nombre_cientifico' id='tabla_nombre_cientifico' value='"
+        revisionHTML += "'/></div><br/><div><label for='tabla_nombre_cientifico' "+\
+            "style='float:left;padding-right:37px;'>Nombre científico:</label>"+\
+            "<input type='text' name='nombre_cientifico' class='string' id='tabla_nombre_cientifico' value='"
 
-    #Si hay nombre científico, éste aparece en la casilla para ingresar el texto.
-    if datosFoto.nombre_cientifico:
+        #Si hay nombre científico, éste aparece en la casilla para ingresar el texto.
+        if datosFoto.nombre_cientifico:
 
-        revisionHTML += datosFoto.nombre_cientifico
+            revisionHTML += datosFoto.nombre_cientifico
 
-    revisionHTML += "'/></div><br/><div><label for='tabla_numero_individuos' "+\
-    "style='float:left;padding-right:10px;'>Número de individuos:</label>"+\
-    "<input type='text' name='numero_individuos' class='integer' id='tabla_numero_individuos' value='"
+        revisionHTML += "'/></div><br/><div><label for='tabla_numero_individuos' "+\
+        "style='float:left;padding-right:10px;'>Número de individuos:</label>"+\
+        "<input type='text' name='numero_individuos' class='integer' id='tabla_numero_individuos' value='"
 
-    if datosFoto.numero_individuos:
+        if datosFoto.numero_individuos:
 
-        revisionHTML += str(datosFoto.numero_individuos)
+            revisionHTML += str(datosFoto.numero_individuos)
 
-    revisionHTML += "'/></div><br/><input type='button' value='Enviar' id='tabla_enviar'/></form>"
+        revisionHTML += "'/></div><br/><input type='button' value='Enviar' id='tabla_enviar'/></form>"
 
 #HTML generado:
 
@@ -106,14 +106,14 @@ def obtenerFotografia():
 #               <label for='tabla_nombre_comun' style='float:left;padding-right:22px;'>
 #                   Nombre común:
 #               </label>
-#               <input type='text' name='nombre_comun' id='tabla_nombre_comun' value=''/>
+#               <input type='text' name='nombre_comun' class='string' id='tabla_nombre_comun' value=''/>
 #           </div>
 #           <br/>
 #           <div>
 #               <label for='tabla_nombre_cientifico' style='float:left;padding-right:10px;'>
 #                   Nombre científico:
 #               </label>
-#               <input type='text' name='nombre_cientifico' id='tabla_nombre_cientifico' value=''/>
+#               <input type='text' name='nombre_cientifico' class='string' id='tabla_nombre_cientifico' value=''/>
 #           </div>
 #           <br/>
 #           <div>
@@ -126,9 +126,9 @@ def obtenerFotografia():
 #           <input type='button' value='Enviar' id='tabla_enviar'/>
 #         </form>
 
-    #except:
+    except:
 
-        #revisionHTML = "<form id='forma_shadow'></form>"
+        revisionHTML = "<form id='forma_shadow'></form>"
     
     return XML(revisionHTML)
 
@@ -145,14 +145,12 @@ def actualizarFotografia():
 
     #Viendo si se encontró fauna evidente, no se encontró o la foto simplemente no fue revisada.
     if faunaEvidente == 'encontrada':
-        faunaEvidente = True
-    elif faunaEvidente == 'no_encontrada':
-        faunaEvidente = False
-    else:
-        faunaEvidente = None
+        db(db.Archivo_camara.id==fotoElegidaID).update(presencia=True,\
+        nombre_comun=nombreComun, nombre_cientifico=nombreCientifico, numero_individuos=numeroIndividuos)
 
-    db(db.Archivo_camara.id==fotoElegidaID).update(presencia=faunaEvidente,\
-     nombre_comun=nombreComun, nombre_cientifico=nombreCientifico, numero_individuos=numeroIndividuos)
+    else:
+        db(db.Archivo_camara.id==fotoElegidaID).update(presencia=False,\
+        nombre_comun=None, nombre_cientifico=None, numero_individuos=None)
 
 def download():
     return response.download(request, db)
