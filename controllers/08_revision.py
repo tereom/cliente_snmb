9e62eeb01bc65f1b21568881ef50068850ee4bbd
fpardourrutia <fpardo@iMac-de-sst.local>
2014-12-08 10:31:53 -0600

    Tratando de incluir el visor de videos en revisión de fauna

--- controllers/08_revision.py
+++ controllers/08_revision.py
@@ -19,11 +19,39 @@ def obtenerFotografia():
     try:
 
         revisionHTML = "<form id='forma_shadow'><input type='hidden' name='id_foto' value='" +\
-            str(datosFoto.id) + "'/><center><img src='/cliente_web2py/08_revision/download/"+datosFoto.archivo+\
-            "' alt='Error al cargar la fotografía' style='width:800px;height:600px;'/></center>"+\
-            "<hr/><div><div style='float:left;padding-right:60px;'><label for='tabla_con_fauna_evidente' "+\
-            "style='float:left;padding-right:20px;'>Con fauna evidente</label><input type='radio' "+\
-            "name='fauna_evidente' value='encontrada' id='tabla_con_fauna_evidente'"
+                str(datosFoto.id) + "'/><center>"
+
+        #Revisando la terminación del archivo para ver si desplegar una imagen
+        #o un video:
+
+        nombre = datosFoto.archivo_nombre_original.split('.')
+        terminacion = nombre[1].lower()
+
+        #Revisando la terminación del archivo:
+
+        if terminacion == 'avi':
+
+            #revisionHTML += "<video src='/init/8_revision/download/"+datosFoto.archivo+\
+            #    "' controls='controls' autoplay='autoplay' style='width:800px;height:600px;'/>"
+
+            revisionHTML += "<object classid='clsid:d27cdb6e-ae6d-11cf-96b8-444553540000'"+\
+                " width='550' height='400' id='movie_name' align='middle'><param name='movie'"+\
+                " value='/init/8_revision/download/"+datosFoto.archivo+"'/><!--[if !IE]>-->"+\
+                "<object type='application/x-shockwave-flash' data='/init/8_revision/download/"+datosFoto.archivo+\
+                "' width='550' height='400'><param name='movie' value='/init/8_revision/download/"+datosFoto.archivo+\
+                "'/><!--<![endif]--><a href='http://www.adobe.com/go/getflash'>"+\
+                "<img src='http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif'"+\
+                " alt='Get Adobe Flash player'/></a><!--[if !IE]>--></object><!--<![endif]--></object>"
+
+        else:
+
+            revisionHTML += "<img src='/cliente_web2py/08_revision/download/"+datosFoto.archivo+\
+                "' alt='Error al cargar la fotografía' style='width:800px;height:600px;'/>"
+
+        revisionHTML +="</center><hr/><div><div style='float:left;padding-right:60px;'><label "+\
+                "for='tabla_con_fauna_evidente' style='float:left;padding-right:20px;'>"+\
+                "Con fauna evidente</label><input type='radio' name='fauna_evidente' "+\
+                "value='encontrada' id='tabla_con_fauna_evidente'"
 
         #Si el campo de presencia de la foto elegida es True, entonces la casilla "fauna evidente" aparece marcada.
 
@@ -49,7 +77,8 @@ def obtenerFotografia():
 
             revisionHTML += "/>"
 
-        revisionHTML += "</div></div><div style='clear:both;'></div><br/><div>"+\
+        revisionHTML += "</div></div><div style='clear:both;'></div>"+\
+            "<!--Para el fade-in/out: <div id='info_fauna'>--><br/><div>"+\
             "<label for='tabla_nombre_comun' style='float:left;padding-right:49px;'>"+\
             "Nombre común:</label><input type='text' name='nombre_comun' class='string' "+\
             "id='tabla_nombre_comun' value='"
@@ -76,7 +105,7 @@ def obtenerFotografia():
 
             revisionHTML += str(datosFoto.numero_individuos)
 
-        revisionHTML += "'/></div><br/><input type='button' value='Enviar' id='tabla_enviar'/></form>"
+        revisionHTML += "'/></div><!--</div>--><br/><input type='button' value='Enviar' id='tabla_enviar'/></form>"
 
 #HTML generado:
 
