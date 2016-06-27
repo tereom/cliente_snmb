f9be01e7c965081d32f05bb0da95de915dd92658
fpardourrutia <fpardo@fpardo-macpro.local>
2016-06-27 18:06:56 -0500

    Se revirtió el código para que sea compatible con Web2py v2.9.11 (Mac)
    Esto debido a que la vista generada al presionar el botón details de
    las tablas generadas con SQLFORM.grid() no se mostraba correctamente.

--- controllers/default.py
+++ controllers/default.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 # this file is released under public domain and you can use without limitations
 
-# -------------------------------------------------------------------------
-# This is a sample controller
-# - index is the default action of any application
-# - user is required for authentication and authorization
-# - download is for downloading files uploaded in the db (does streaming)
-# -------------------------------------------------------------------------
+#########################################################################
+## This is a sample controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+## - api is an example of Hypermedia API support and access control
+#########################################################################
 
 def index():
     """
@@ -16,7 +17,6 @@ def index():
     if you need a simple wiki simply replace the two lines below with:
     return auth.wiki()
     """
-
     #########################################################################
     #########################################################################
     # <Editado para el cliente>
@@ -41,12 +41,11 @@ def user():
     http://..../[app]/default/user/profile
     http://..../[app]/default/user/retrieve_password
     http://..../[app]/default/user/change_password
-    http://..../[app]/default/user/bulk_register
+    http://..../[app]/default/user/manage_users (requires membership in
     use @auth.requires_login()
         @auth.requires_membership('group name')
         @auth.requires_permission('read','table name',record_id)
     to decorate functions that need access control
-    also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
     """
     return dict(form=auth())
 
@@ -68,3 +67,16 @@ def call():
     supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
     """
     return service()
+
+
+@auth.requires_login() 
+def api():
+    """
+    this is example of API with access control
+    WEB2PY provides Hypermedia API (Collection+JSON) Experimental
+    """
+    from gluon.contrib.hypermedia import Collection
+    rules = {
+        '<tablename>': {'GET':{},'POST':{},'PUT':{},'DELETE':{}},
+        }
+    return Collection(db).process(request,response,rules)
--- models/menu.py
+++ models/menu.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # this file is released under public domain and you can use without limitations
 
-# ----------------------------------------------------------------------------------------------------------------------
-# Customize your APP title, subtitle and menus here
-# ----------------------------------------------------------------------------------------------------------------------
+#########################################################################
+## Customize your APP title, subtitle and menus here
+#########################################################################
 
 #########################################################################
 #########################################################################
@@ -11,9 +11,8 @@
 #########################################################################
 #########################################################################
 
-# response.logo = A(B('web', SPAN(2), 'py'), XML('&trade;&nbsp;'),
-#                   _class="navbar-brand", _href="http://www.web2py.com/",
-#                   _id="web2py-logo")
+# response.logo = A(B('web',SPAN(2),'py'),XML('&trade;&nbsp;'),
+#                  _class="brand",_href="http://www.web2py.com/")
 
 #########################################################################
 #########################################################################
@@ -21,25 +20,20 @@
 #########################################################################
 #########################################################################
 
-response.title = request.application.replace('_', ' ').title()
+response.title = request.application.replace('_',' ').title()
 response.subtitle = ''
 
-# ----------------------------------------------------------------------------------------------------------------------
-# read more at http://dev.w3.org/html5/markup/meta.name.html
-# ----------------------------------------------------------------------------------------------------------------------
-response.meta.author = myconf.get('app.author')
-response.meta.description = myconf.get('app.description')
-response.meta.keywords = myconf.get('app.keywords')
-response.meta.generator = myconf.get('app.generator')
+## read more at http://dev.w3.org/html5/markup/meta.name.html
+response.meta.author = 'Your Name <you@example.com>'
+response.meta.keywords = 'web2py, python, framework'
+response.meta.generator = 'Web2py Web Framework'
 
-# ----------------------------------------------------------------------------------------------------------------------
-# your http://google.com/analytics id
-# ----------------------------------------------------------------------------------------------------------------------
+## your http://google.com/analytics id
 response.google_analytics_id = None
 
-# ----------------------------------------------------------------------------------------------------------------------
-# this is the main application menu add/remove items as required
-# ----------------------------------------------------------------------------------------------------------------------
+#########################################################################
+## this is the main application menu add/remove items as required
+#########################################################################
 
 #########################################################################
 #########################################################################
@@ -81,13 +75,13 @@ response.menu = [
     # Carbono
     ###########################################
 
-    (T('Vegetación y suelo'), False, URL('07_vegetacion_suelo', 'index1'), [
-      (T('Material leñoso caído'), False, URL('07_vegetacion_suelo', 'index1')),
-      (T('Material 1000h'), False, URL('07_vegetacion_suelo', 'index2')),
-      (T('Carbono en el mantillo'), False, URL('07_vegetacion_suelo', 'index3')),
-      (T('Árboles pequeños y arbustos'), False, URL('07_vegetacion_suelo', 'index4')),
-      (T('Árboles grandes'), False, URL('07_vegetacion_suelo', 'index5')),
-      (T('Epífitas'), False, URL('07_vegetacion_suelo', 'index6'))]),
+    # (T('Vegetación y suelo'), False, URL('07_vegetacion_suelo', 'index1'), [
+    #   (T('Material leñoso caído'), False, URL('07_vegetacion_suelo', 'index1')),
+    #   (T('Material 1000h'), False, URL('07_vegetacion_suelo', 'index2')),
+    #   (T('Carbono en el mantillo'), False, URL('07_vegetacion_suelo', 'index3')),
+    #   (T('Árboles pequeños y arbustos'), False, URL('07_vegetacion_suelo', 'index4')),
+    #   (T('Árboles grandes'), False, URL('07_vegetacion_suelo', 'index5')),
+    #   (T('Epífitas'), False, URL('07_vegetacion_suelo', 'index6'))]),
 
     # ### Impactos ambientales
     # (T('Impactos ambientales'),False,URL('13_impactos_ambientales','index3'), [
@@ -174,117 +168,112 @@ DEVELOPMENT_MENU = False
 #########################################################################
 #########################################################################
 
-# ----------------------------------------------------------------------------------------------------------------------
-# provide shortcuts for development. remove in production
-# ----------------------------------------------------------------------------------------------------------------------
+#########################################################################
+## provide shortcuts for development. remove in production
+#########################################################################
 
 def _():
-    # ------------------------------------------------------------------------------------------------------------------
     # shortcuts
-    # ------------------------------------------------------------------------------------------------------------------
     app = request.application
     ctr = request.controller
-    # ------------------------------------------------------------------------------------------------------------------
     # useful links to internal and external resources
-    # ------------------------------------------------------------------------------------------------------------------
     response.menu += [
+        (SPAN('web2py', _class='highlighted'), False, 'http://web2py.com', [
         (T('My Sites'), False, URL('admin', 'default', 'site')),
-        (T('This App'), False, '#', [
-            (T('Design'), False, URL('admin', 'default', 'design/%s' % app)),
-            LI(_class="divider"),
-            (T('Controller'), False,
-             URL(
-                 'admin', 'default', 'edit/%s/controllers/%s.py' % (app, ctr))),
-            (T('View'), False,
-             URL(
-                 'admin', 'default', 'edit/%s/views/%s' % (app, response.view))),
-            (T('DB Model'), False,
-             URL(
-                 'admin', 'default', 'edit/%s/models/db.py' % app)),
-            (T('Menu Model'), False,
-             URL(
-                 'admin', 'default', 'edit/%s/models/menu.py' % app)),
-            (T('Config.ini'), False,
-             URL(
-                 'admin', 'default', 'edit/%s/private/appconfig.ini' % app)),
-            (T('Layout'), False,
-             URL(
-                 'admin', 'default', 'edit/%s/views/layout.html' % app)),
-            (T('Stylesheet'), False,
-             URL(
-                 'admin', 'default', 'edit/%s/static/css/web2py-bootstrap3.css' % app)),
-            (T('Database'), False, URL(app, 'appadmin', 'index')),
-            (T('Errors'), False, URL(
-                'admin', 'default', 'errors/' + app)),
-            (T('About'), False, URL(
-                'admin', 'default', 'about/' + app)),
+        (T('This App'), False, URL('admin', 'default', 'design/%s' % app), [
+        (T('Controller'), False,
+         URL(
+         'admin', 'default', 'edit/%s/controllers/%s.py' % (app, ctr))),
+        (T('View'), False,
+         URL(
+         'admin', 'default', 'edit/%s/views/%s' % (app, response.view))),
+        (T('Layout'), False,
+         URL(
+         'admin', 'default', 'edit/%s/views/layout.html' % app)),
+        (T('Stylesheet'), False,
+         URL(
+         'admin', 'default', 'edit/%s/static/css/web2py.css' % app)),
+        (T('DB Model'), False,
+         URL(
+         'admin', 'default', 'edit/%s/models/db.py' % app)),
+        (T('Menu Model'), False,
+         URL(
+         'admin', 'default', 'edit/%s/models/menu.py' % app)),
+        (T('Database'), False, URL(app, 'appadmin', 'index')),
+        (T('Errors'), False, URL(
+         'admin', 'default', 'errors/' + app)),
+        (T('About'), False, URL(
+         'admin', 'default', 'about/' + app)),
         ]),
-        ('web2py.com', False, '#', [
-            (T('Download'), False,
-             'http://www.web2py.com/examples/default/download'),
-            (T('Support'), False,
-             'http://www.web2py.com/examples/default/support'),
-            (T('Demo'), False, 'http://web2py.com/demo_admin'),
-            (T('Quick Examples'), False,
-             'http://web2py.com/examples/default/examples'),
-            (T('FAQ'), False, 'http://web2py.com/AlterEgo'),
-            (T('Videos'), False,
-             'http://www.web2py.com/examples/default/videos/'),
-            (T('Free Applications'),
-             False, 'http://web2py.com/appliances'),
-            (T('Plugins'), False, 'http://web2py.com/plugins'),
-            (T('Recipes'), False, 'http://web2pyslices.com/'),
-        ]),
-        (T('Documentation'), False, '#', [
-            (T('Online book'), False, 'http://www.web2py.com/book'),
-            LI(_class="divider"),
-            (T('Preface'), False,
-             'http://www.web2py.com/book/default/chapter/00'),
-            (T('Introduction'), False,
-             'http://www.web2py.com/book/default/chapter/01'),
-            (T('Python'), False,
-             'http://www.web2py.com/book/default/chapter/02'),
-            (T('Overview'), False,
-             'http://www.web2py.com/book/default/chapter/03'),
-            (T('The Core'), False,
-             'http://www.web2py.com/book/default/chapter/04'),
-            (T('The Views'), False,
-             'http://www.web2py.com/book/default/chapter/05'),
-            (T('Database'), False,
-             'http://www.web2py.com/book/default/chapter/06'),
-            (T('Forms and Validators'), False,
-             'http://www.web2py.com/book/default/chapter/07'),
-            (T('Email and SMS'), False,
-             'http://www.web2py.com/book/default/chapter/08'),
-            (T('Access Control'), False,
-             'http://www.web2py.com/book/default/chapter/09'),
-            (T('Services'), False,
-             'http://www.web2py.com/book/default/chapter/10'),
-            (T('Ajax Recipes'), False,
-             'http://www.web2py.com/book/default/chapter/11'),
-            (T('Components and Plugins'), False,
-             'http://www.web2py.com/book/default/chapter/12'),
-            (T('Deployment Recipes'), False,
-             'http://www.web2py.com/book/default/chapter/13'),
-            (T('Other Recipes'), False,
-             'http://www.web2py.com/book/default/chapter/14'),
-            (T('Helping web2py'), False,
-             'http://www.web2py.com/book/default/chapter/15'),
-            (T("Buy web2py's book"), False,
-             'http://stores.lulu.com/web2py'),
-        ]),
-        (T('Community'), False, None, [
-            (T('Groups'), False,
-             'http://www.web2py.com/examples/default/usergroups'),
-            (T('Twitter'), False, 'http://twitter.com/web2py'),
-            (T('Live Chat'), False,
-             'http://webchat.freenode.net/?channels=web2py'),
-        ]),
-    ]
+            ('web2py.com', False, 'http://www.web2py.com', [
+             (T('Download'), False,
+              'http://www.web2py.com/examples/default/download'),
+             (T('Support'), False,
+              'http://www.web2py.com/examples/default/support'),
+             (T('Demo'), False, 'http://web2py.com/demo_admin'),
+             (T('Quick Examples'), False,
+              'http://web2py.com/examples/default/examples'),
+             (T('FAQ'), False, 'http://web2py.com/AlterEgo'),
+             (T('Videos'), False,
+              'http://www.web2py.com/examples/default/videos/'),
+             (T('Free Applications'),
+              False, 'http://web2py.com/appliances'),
+             (T('Plugins'), False, 'http://web2py.com/plugins'),
+             (T('Layouts'), False, 'http://web2py.com/layouts'),
+             (T('Recipes'), False, 'http://web2pyslices.com/'),
+             (T('Semantic'), False, 'http://web2py.com/semantic'),
+             ]),
+            (T('Documentation'), False, 'http://www.web2py.com/book', [
+             (T('Preface'), False,
+              'http://www.web2py.com/book/default/chapter/00'),
+             (T('Introduction'), False,
+              'http://www.web2py.com/book/default/chapter/01'),
+             (T('Python'), False,
+              'http://www.web2py.com/book/default/chapter/02'),
+             (T('Overview'), False,
+              'http://www.web2py.com/book/default/chapter/03'),
+             (T('The Core'), False,
+              'http://www.web2py.com/book/default/chapter/04'),
+             (T('The Views'), False,
+              'http://www.web2py.com/book/default/chapter/05'),
+             (T('Database'), False,
+              'http://www.web2py.com/book/default/chapter/06'),
+             (T('Forms and Validators'), False,
+              'http://www.web2py.com/book/default/chapter/07'),
+             (T('Email and SMS'), False,
+              'http://www.web2py.com/book/default/chapter/08'),
+             (T('Access Control'), False,
+              'http://www.web2py.com/book/default/chapter/09'),
+             (T('Services'), False,
+              'http://www.web2py.com/book/default/chapter/10'),
+             (T('Ajax Recipes'), False,
+              'http://www.web2py.com/book/default/chapter/11'),
+             (T('Components and Plugins'), False,
+              'http://www.web2py.com/book/default/chapter/12'),
+             (T('Deployment Recipes'), False,
+              'http://www.web2py.com/book/default/chapter/13'),
+             (T('Other Recipes'), False,
+              'http://www.web2py.com/book/default/chapter/14'),
+             (T('Buy this book'), False,
+              'http://stores.lulu.com/web2py'),
+             ]),
+            (T('Community'), False, None, [
+             (T('Groups'), False,
+              'http://www.web2py.com/examples/default/usergroups'),
+                        (T('Twitter'), False, 'http://twitter.com/web2py'),
+                        (T('Live Chat'), False,
+                         'http://webchat.freenode.net/?channels=web2py'),
+                        ]),
+                (T('Plugins'), False, None, [
+                        ('plugin_wiki', False,
+                         'http://web2py.com/examples/default/download'),
+                        (T('Other Plugins'), False,
+                         'http://web2py.com/plugins'),
+                        (T('Layout Plugins'),
+                         False, 'http://web2py.com/layouts'),
+                        ])
+                ]
+         )]
+if DEVELOPMENT_MENU: _()
 
-
-if DEVELOPMENT_MENU:
-    _()
-
-if "auth" in locals():
-    auth.wikimenu()
+if "auth" in locals(): auth.wikimenu() 
